c5d29e0d636bb14a52c3c755236b02012d2a02e4
David Soares <2221403@my.ipleiria.pt>
2024-04-18 10:24:14 +0100

    bruh

--- src/main/java/pt/ipleiria/estg/dei/ei/esoft/Main.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/esoft/Main.java
@@ -1,18 +1,13 @@
 package pt.ipleiria.estg.dei.ei.esoft;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 
 public class Main extends JFrame {
     private JPanel panel1;
     private JButton btnSair;
     private JButton btnDadosPessoais;
-    private JButton dadosEstatisticosButton;
+    private JButton btnDadosEstatisticos;
     private JButton contratosButton;
     private JPanel painelPrincipal;
 
@@ -25,7 +20,9 @@ public class Main extends JFrame {
 
         btnSair.addActionListener(this::btnSairActionPerformed);
         btnDadosPessoais.addActionListener(this::btnDadosPessoaisActionPerformed);
+        btnDadosEstatisticos.addActionListener(this::btnDadosEstatisticosPerformed);
     }
+
     private void btnSairActionPerformed(ActionEvent e) {
         System.exit(0);
     }
@@ -33,6 +30,10 @@ public class Main extends JFrame {
     private void btnDadosPessoaisActionPerformed(ActionEvent e) {
         var janela = new JanelaDadosPessoais();
         janela.setVisible(true);
+    }
+
+    private void btnDadosEstatisticosPerformed(ActionEvent e) {
+
     }
 
     public static void main(String[] args) {
