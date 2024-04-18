a6f92e1cadd44bd06710c9a070cb812c21471d7f
2221858 <2221858@my.ipleiria.pt>
2024-04-18 10:05:15 +0100

    ola

--- src/main/java/pt/ipleiria/estg/dei/ei/esoft/JanelaDadosPessoais.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/esoft/JanelaDadosPessoais.java
@@ -1,15 +1,24 @@
 package pt.ipleiria.estg.dei.ei.esoft;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class JanelaDadosPessoais extends JFrame {
     private JPanel panel1;
     private JButton btnContratar;
     private JButton btnDespedir;
     private JPanel painelDadosPessoais;
+    private DadosPessoaisJogadores dados;
+
+
 
     public JanelaDadosPessoais()
     {
+        dados = new DadosPessoaisJogadores();
 
         setContentPane(painelDadosPessoais);
         // Destrói esta janela, removendo-a completamente da memória.
@@ -18,4 +27,40 @@ public class JanelaDadosPessoais extends JFrame {
         pack();
         setVisible(true);
     }
+
+    private void guardarDadosDisco() {
+        ObjectOutputStream oos = null;
+        try {
+            File f =new
+                    File(System.getProperty("user.home")+File.separator+"minesfinder.recordes");
+            oos = new ObjectOutputStream(new FileOutputStream(f));
+            oos.writeObject(recordesFacil);
+            oos.close();
+        } catch (IOException ex) {
+            Logger.getLogger(JanelaDadosPessoais.class.getName()).log(Level.SEVERE, null,
+                    ex);
+        }
+    }
+
+    private void lerDados() {
+        ObjectInputStream ois = null;
+        File f = new
+                File(System.getProperty("user.home")+File.separator+"GestorEquipas.dadosPessoais");
+        if (f.canRead()) {
+            try {
+                ois = new ObjectInputStream(new FileInputStream(f));
+                recordesFacil=(TabelaRecordes) ois.readObject();
+                recordesMedio=(TabelaRecordes) ois.readObject();
+                recordesDificil=(TabelaRecordes) ois.readObject();
+                ois.close();
+            } catch (IOException ex) {
+                Logger.getLogger(MinesFinder.class.getName()).log(Level.SEVERE,
+                        null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(MinesFinder.class.getName()).log(Level.SEVERE,
+                        null, ex);
+            }
+        }
+    }
+
 }
